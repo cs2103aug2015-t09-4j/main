00c5af73645ee110de4587d4914fe4272053ad30
lemonaoko <lemonaoko@gmail.com>
2015-10-06 22:22:24 +0800

    store and read object

--- src/FileStorage.java
+++ src/FileStorage.java
@@ -2,97 +2,44 @@ import java.io.*;
 
 public class FileStorage {
 	
-	public FileStorage() {
-		
-	}
-	
-	public static void addEvent(String command) {
-		try {
-			BufferedWriter textWriter = new BufferedWriter(new FileWriter("test.txt", true));
-			
-			textWriter.write(command.substring(4));
-			textWriter.newLine();
-			
-			textWriter.close();
-			
-			GUIConsole.successfulAdd(command.substring(4));
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void deleteEvent(String command) {
-		File tempFile = new File("temp.txt");
-		File inputFile = new File("test.txt");
-		Integer lineToBeDelete = Integer.parseInt(command.substring(7));
-		Integer lineCount = 1;
-		String aLineOfContent;
-		
-		try {
-			BufferedReader textReader = new BufferedReader(new FileReader(inputFile));
-			BufferedWriter textWriter = new BufferedWriter(new FileWriter(tempFile, true));
-			
-			while((aLineOfContent = textReader.readLine()) != null) {
-				if(lineCount != lineToBeDelete) {
-					textWriter.write(aLineOfContent);
-				}
-				lineCount++;
-			}
-			
-			textReader.close();
-			textWriter.close();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		inputFile.delete();
-		tempFile.renameTo(inputFile);
-		
-	}
-	
-	public static void recurEvent(String command) {
-		//TBC
-		GUIConsole.successfulRecur("TBC");
-	}
-	
-	public static void navigateDay(String command) {
-		//TBC
-		GUIConsole.successfulNavigate("TBC");
-	}
-
-	public static void editEvent(String command) {
-		File tempFile = new File("temp.txt");
-		File inputFile = new File("test.txt");
-		Integer lineToBeEdit = Integer.parseInt(command.substring(5, 6));
-		Integer lineCount = 1;
-		String aLineOfContent;
-		String replacement = command.substring(7);
-		
-		try {
-			BufferedReader textReader = new BufferedReader(new FileReader(inputFile));
-			BufferedWriter textWriter = new BufferedWriter(new FileWriter(tempFile, true));
-			
-			while((aLineOfContent = textReader.readLine()) != null) {
-				if(lineCount != lineToBeEdit) {
-					textWriter.write(aLineOfContent);
-				}
-				else {
-					textWriter.write(replacement);
-				}
-				lineCount++;
-			}
-			
-			textReader.close();
-			textWriter.close();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		inputFile.delete();
-		tempFile.renameTo(inputFile);
-		
-	}		
-	
+	private void saveObject(String path, Object obj) {   
+        FileOutputStream fos = null;  
+        ObjectOutputStream oos = null;  
+        File f = new File(path);  
+        try {  
+            fos = new FileOutputStream(f);  
+            oos = new ObjectOutputStream(fos);  
+            oos.writeObject(obj); 
+        	oos.flush();
+            oos.close();
+            fos.flush();
+            fos.close();  
+        } catch (FileNotFoundException e) {  
+            e.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        }  
+    }  
+      
+    private Object readObject(String path){  
+    	Object temp = null;
+        FileInputStream fis = null;  
+        ObjectInputStream ois = null;     
+        File f = new File(path);  
+        try {  
+            fis = new FileInputStream(f);  
+            ois = new ObjectInputStream(fis);  
+            temp = ois.readObject();
+            ois.close();  
+            fis.close();  
+        } catch (FileNotFoundException e) {  
+            e.printStackTrace();  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        } catch (ClassNotFoundException e) {  
+            e.printStackTrace();  
+        } finally{  
+            return temp;
+        }  
+    }  
 }
