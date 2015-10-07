47324f35f8c1d0a2c241aafd2a7262540ff9ae79
Xasaza <xa_Saza@hotmail.com>
2015-10-08 00:18:42 +0800

    Linked parser to UI

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -11,10 +11,12 @@ public class CommandParser {
 			newTask.setTaskType(eventType);
 			newTask.setTaskDate(Integer.valueOf(commandParts[2]));
 			newTask.setTaskName(commandParts[3]);
+			GUIConsole.successfulAdd(commandParts[3]);
 			break;
 		case "floating": 
 			newTask.setTaskType(eventType);
 			newTask.setTaskName(commandParts[1]);
+			GUIConsole.successfulAdd(commandParts[1]);
 			break;
 		case "event": 
 			newTask.setTaskType(eventType);
@@ -22,6 +24,7 @@ public class CommandParser {
 			newTask.setTaskStartTime(Integer.valueOf(commandParts[3]));
 			newTask.setTaskEndTime(Integer.valueOf(commandParts[4]));
 			newTask.setTaskName(commandParts[5]);
+			GUIConsole.successfulAdd(commandParts[5]);
 			break;
 		}
 	}
@@ -34,13 +37,16 @@ public class CommandParser {
 			switch(commandParts[i++]){
 			case "name":
 				newTask.setTaskName(commandParts[i++]);
+				GUIConsole.successfulEditName(commandParts[1], commandParts[i]);
 				break;
 			case "date":
 				newTask.setTaskDate(Integer.valueOf(commandParts[i++]));
+				GUIConsole.successfulEditDate(commandParts[1], Integer.commandParts[i]);
 				break;
 			case "time":
 				newTask.setTaskStartTime(Integer.valueOf(commandParts[i++]));
 				newTask.setTaskEndTime(Integer.valueOf(commandParts[i++]));
+				GUIConsole.successfulEditTime(commandParts[1], Integer.commandParts[i], Integer.commandParts[i--]);
 				break;
 			}
 		}
@@ -50,6 +56,7 @@ public class CommandParser {
 	public void parseDelete(String[] commandParts) {
 		String taskType = commandParts[1];
 		String taskName = commandParts[2];
+		GUIConsole.successfulDelete(commandParts[2]);
 
 	}
 
