4ea5223f837a1b7733531aed7e2e0455237aa219
lemonaoko <lemonaoko@gmail.com>
2015-10-07 22:33:07 +0800

    final version
    need change to Task class (overwrite the toString function) and set
    additional class person to refer to

--- src/FileStorage.java
+++ src/FileStorage.java
@@ -1,3 +1,4 @@
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -7,37 +8,48 @@ import java.util.ArrayList;
 
 public class FileStorage {
 	
-	static int count = 0;
-
-     public static void main(String[] args) throws Exception {
-         String path = "d:" + File.separator + "test.txt";
-         File f = new File(path);
-         Person p1 = new Person("张三", 23);
-         write(f, p1);
-         read(f, count);
-         Person p2 = new Person("李四", 32);
-         write(f, p2); 
-         read(f, count);
-     }
+	static ArrayList<Task> objectList = new ArrayList<Task>();
+	
+    public static void main(String[] args) throws Exception {
+        String path = "d:" + File.separator + "test.txt";
+        File f = new File(path);
+        Person p1 = new Person("a", 23);
+        write(f, p1);
+        read(f);
+        Person p2 = new Person("c", 32);
+        write(f, p2); 
+        read(f);
+    }
 
  
-     public static void write(File f, Object p) throws Exception { 
-         OutputStream out = new FileOutputStream(f, true);
-         MyObjectOutputStream oos = MyObjectOutputStream.newInstance(f, out);
-         oos.writeObject(p);
-         count++;
-         oos.close();
-     }
+    public static void write(File f, Object p) throws Exception {
+    	 
+        OutputStream out = new FileOutputStream(f, true);
+        MyObjectOutputStream oos = MyObjectOutputStream.newInstance(f, out);
+        oos.writeObject(p);
+        oos.close();
+        System.out.println("-----Oject write successfully....");
+    }
 
-	public static ArrayList<Object> read(File f, int count) throws Exception {
-    	 ArrayList<Object> objectList = new ArrayList<Object>();
+     public static ArrayList<Task> read(File f) throws Exception {
          FileInputStream in = new FileInputStream(f);
          ObjectInputStream ois = new ObjectInputStream(in);
-         for (int i = 0; i < count; i++) {
-        	  objectList.add(i, ois.readObject());
-              System.out.println((Person) ois.readObject());
-         }
-         ois.close();
-         return objectList;
+         int i = 0;
+
+         while(true) {   
+        	 try{
+        	  Task temp = new Task();
+        	  temp = (Task) ois.readObject();
+        	  objectList.add(i, temp);
+        	  i++;
+              System.out.println((Task)temp);
+        	 } catch (EOFException e){
+        		 break;
+        	 }
+        
+         } 
+
+        ois.close();
+		return objectList;
      }
 }
---
+++ src/Person.java
@@ -0,0 +1,16 @@
+import java.io.Serializable;
+
+class Person implements Serializable {
+     private static final long serialVersionUID = 1L;
+     private String name;
+     private int age;
+     public Person() {
+     }
+     public Person(String name, int age) {
+         this.name = name;
+         this.age = age;
+     }
+     public String toString() {
+         return "ĞÕÃû£º" + name + ";ÄêÁä£º" + age;
+     }
+}
