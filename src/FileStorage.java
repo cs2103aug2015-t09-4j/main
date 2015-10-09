937445d413adef440c996b54d9bf967bcf0ccf3e
lemonaoko <lemonaoko@gmail.com>
2015-10-10 04:54:48 +0800

    delete unused class

--- src/FileStorage.java
+++ src/FileStorage.java
@@ -2,6 +2,7 @@ import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
@@ -11,19 +12,6 @@ public class FileStorage{
 //	private static final long serialVersionUID = -769626947865283;
 	static ArrayList<Task> objectList = new ArrayList<Task>();
 	
-	/*
-	 * refer to Person class and this is an example
-    public static void main(String[] args) throws Exception {
-        String path = "d:" + File.separator + "test.txt";
-        File f = new File(path);
-        Person p1 = new Person("a", 23);
-        write(f, p1);
-        Person p2 = new Person("c", 32);
-        write(f, p2); 
-        read(f);
-    }
-    */
-	
 	public static void clear(File f) throws Exception {
 		OutputStream out = new FileOutputStream(f, false);
 		MyObjectOutputStream oos = MyObjectOutputStream.newInstance(f, out);
@@ -31,7 +19,7 @@ public class FileStorage{
 		//out.flush();
 	}
  
-    public static void write(File f, Object p) throws Exception {
+    public static void write(File f, Object p) throws IOException {
     	 
         OutputStream out = new FileOutputStream(f, true);
         MyObjectOutputStream oos = MyObjectOutputStream.newInstance(f, out);
@@ -40,7 +28,7 @@ public class FileStorage{
         System.out.println("-----Oject write successfully....");
     }
 
-     public static ArrayList<Task> read(File f) throws Exception {
+     public static ArrayList<Task> read(File f) throws IOException {
     	 objectList = new ArrayList<Task>();
          FileInputStream in = new FileInputStream(f);
          ObjectInputStream ois = new ObjectInputStream(in);
