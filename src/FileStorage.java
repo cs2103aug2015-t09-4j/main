0bf9008fe43ab87d49ee9e3f58489994ebc5fb93
lemonaoko <lemonaoko@gmail.com>
2015-10-14 16:23:37 +0800

    modify storage
    store string and retrieve object

--- src/FileStorage.java
+++ src/FileStorage.java
@@ -1,53 +1,51 @@
+import java.io.BufferedReader;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class FileStorage{
+public class FileStorage extends StorageParser {
 //	private static final long serialVersionUID = -769626947865283;
 	static ArrayList<Task> objectList = new ArrayList<Task>();
+	private static final String MSG_WHEN_INVALID_FILENAME = "cannot find targeted file"; 
 	
 	public static void clear(File f) throws Exception {
 		OutputStream out = new FileOutputStream(f, false);
 		MyObjectOutputStream oos = MyObjectOutputStream.newInstance(f, out);
 		oos.close();
-		//out.flush();
+		out.flush();
 	}
  
-    public static void write(File f, Object p) throws IOException {
-    	 
-        OutputStream out = new FileOutputStream(f, true);
-        MyObjectOutputStream oos = MyObjectOutputStream.newInstance(f, out);
-        oos.writeObject(p);
-        oos.close();
-        System.out.println("-----Oject write successfully....");
+    public static void write(String path, Object p) throws IOException {  
+    	File f = new File(path);
+    	FileWriter fw = new FileWriter(f);
+    	String content = p.toString();
+    	fw.write(content);
     }
 
-     public static ArrayList<Task> read(File f) throws IOException, ClassNotFoundException {
+     public static ArrayList<Task> read(String path) throws IOException, ClassNotFoundException {
     	 objectList = new ArrayList<Task>();
-         FileInputStream in = new FileInputStream(f);
-         ObjectInputStream ois = new ObjectInputStream(in);
-         int i = 0;
-
-         while(true) {   
-        	 try{
-        	  Task temp = new Task();
-        	  temp = (Task) ois.readObject();
-        	  objectList.add(i, temp);
-        	  i++;
-              //System.out.println((Task)temp);
-        	 } catch (EOFException e){
-        		 break;
+         File f = new File(path);
+         if(f.isFile() && f.exists()) {
+        	 FileInputStream fis = new FileInputStream(f);
+        	 InputStreamReader read = new InputStreamReader(fis);
+        	 BufferedReader br = new BufferedReader(read);
+        	 String lineText = null;
+        	 while((lineText = br.readLine()) != null) {
+        		 objectList.add(createTaskFromInformation(lineText));
         	 }
-        
-         } 
-
-        ois.close();
-		return objectList;
+        	 read.close();
+         } else {
+        	 System.out.println(MSG_WHEN_INVALID_FILENAME);
+         }
+		return objectList;       
      }
 }
+
