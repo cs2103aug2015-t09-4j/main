cb9f1e25df0cff73b29b4c11636095efd182a002
lemonaoko <lemonaoko@gmail.com>
2015-11-05 10:29:18 +0800

    edit sort class

--- src/LemonBuddy/Sort.java
+++ src/LemonBuddy/Sort.java
@@ -1,21 +1,45 @@
 package LemonBuddy;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
-class Sort implements Comparator<Object> {
-	 public int compare(Object o1, Object o2) {
-		 Task t1 = (Task)o1;
-		 Task t2 = (Task)o2;
-		 Integer end1, end2, p1, p2;
-		 end1 = Integer.valueOf(t1.getTaskEndDate()).intValue();
-		 end2 = Integer.valueOf(t2.getTaskEndDate()).intValue();
-		 p1 = Integer.valueOf(t1.getTaskPriority()).intValue();
-		 p2 = Integer.valueOf(t2.getTaskPriority()).intValue();
-		 int flag = end1.compareTo(end2);
-		 if(flag == 0) {
-			 return p1.compareTo(p2);
-		 } else {
-			 return flag;
-		 }
-	 }
+class Sort {
+	public static ArrayList<Task> sortByTime(ArrayList<Task> sortList) {
+		Comparator<Task> comparator = new Comparator<Task>(){
+			public int compare(Task t1, Task t2) {
+				String type1, type2;
+				Integer time1, time2;
+				type1 = t1.getTaskType();
+				type2 = t2.getTaskType();
+				
+				if(type1 == "deadline") {
+					time1 = t1.getTaskEndTime();
+				} else if(type1 == "event") {
+					time1 = t1.getTaskStartTime();
+				} else {
+					time1 = null;
+				}
+		 
+				if(type2 == "deadline") {
+					time2 = t2.getTaskEndTime();
+				} else if(type2 == "event") {
+					time2 = t2.getTaskStartTime();
+				} else {
+					time2 = null;
+				}
+				
+				if(time1 > time2) {
+					return 1;
+				} else if(time1 == time2) {
+					return 0;
+				} else {
+					return -1;
+				}
+			}
+		};
+		
+		Collections.sort(sortList, comparator);
+		return sortList;
+	}
 }
