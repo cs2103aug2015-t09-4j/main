ba8a6412dd7ebc27af9d8ff58aaf67854e3b2246
xenzil <xenzil@hotmail.com>
2015-11-09 03:21:26 +0800

    created timehandler interface for handling time stuff in mostly parser

--- src/LemonBuddy/Parser.java
+++ src/LemonBuddy/Parser.java
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class Parser {
+public class Parser extends TimeHandler {
 
 	private static final String KEYWORD_TOMORROW = "tomorrow";
 	private static final String KEYWORD_BY = "by";
@@ -17,6 +17,7 @@ public class Parser {
 	private static final String TASKTYPE_FLOATING = "floating";
 	private static final String TASKTYPE_DEADLINE = "deadline";
 	private static final String TASKTYPE_EVENT = "event";
+	private static TimeHandler timehandler = new TimeHandler();
 
 	public Task parseTask(String[] commandParts) throws Exception {
 		Task newTask = new Task();
@@ -147,22 +148,7 @@ public class Parser {
 		return wordIndex;
 	}
 
-	private void checkValidDateTimeInput(Task newTask) throws ParseException, Exception {
-		if (newTask.getTaskType().equals(TASKTYPE_EVENT)) {
-			SimpleDateFormat sdf = new SimpleDateFormat("ddMMyy");
-			Date date1 = sdf.parse(newTask.getTaskStartDate());
-			Date date2 = sdf.parse(newTask.getTaskEndDate());
-			if (date1.after(date2)) {
-				throw new Exception("Start date after end date");
-			}
-
-			if (newTask.getTaskStartDate() == newTask.getTaskEndDate()) {
-				if (Integer.parseInt(newTask.getTaskStartTime()) > Integer.parseInt(newTask.getTaskEndTime())) {
-					throw new Exception("Start time is after end time");
-				}
-			}
-		}
-	}
+	
 
 	private void setInputTime(String[] commandParts, Task newTask, int wordIndex) throws Exception {
 		String[] timeInfo = new String[2];
@@ -202,7 +188,7 @@ public class Parser {
 					}
 				}
 				timeInfo = getTimeInfo(commandParts, newTask, wordIndex);
-			
+
 				newTask.setTaskEndDate(timeInfo[0]);
 				newTask.setTaskEndTime(timeInfo[1]);
 				newTask.setTaskType(TASKTYPE_EVENT);
@@ -214,8 +200,8 @@ public class Parser {
 
 	private void setOnDefaultTime(Task newTask) {
 		if (newTask.taskStartDateIsEmpty() && newTask.taskEndDateIsEmpty()) {
-			newTask.setTaskStartDate(getCurrentDate());
-			newTask.setTaskEndDate(getCurrentDate());
+			newTask.setTaskStartDate(timehandler.getCurrentDate());
+			newTask.setTaskEndDate(timehandler.getCurrentDate());
 		}
 		if (newTask.taskStartDateIsEmpty() && !newTask.taskEndDateIsEmpty()) {
 			newTask.setTaskStartDate(newTask.getTaskEndDate());
@@ -224,8 +210,8 @@ public class Parser {
 			newTask.setTaskEndDate(newTask.getTaskStartDate());
 		}
 		if (newTask.taskStartTimeIsEmpty() && newTask.taskEndTimeIsEmpty()) {
-			newTask.setTaskStartTime(getCurrentTime());
-			newTask.setTaskEndTime(getCurrentTime());
+			newTask.setTaskStartTime(timehandler.getCurrentTime());
+			newTask.setTaskEndTime(timehandler.getCurrentTime());
 		}
 	}
 
@@ -252,10 +238,10 @@ public class Parser {
 	private void setDeadlineDefaultTime(Task newTask) {
 		if (newTask.getTaskType().equals(TASKTYPE_DEADLINE)) {
 			if (newTask.taskStartDateIsEmpty() && newTask.taskEndDateIsEmpty()) {
-				newTask.setTaskEndDate(getCurrentDate());
+				newTask.setTaskEndDate(timehandler.getCurrentDate());
 			}
 			if (newTask.taskStartTimeIsEmpty() && newTask.taskEndTimeIsEmpty()) {
-				newTask.setTaskEndTime(getCurrentTime());
+				newTask.setTaskEndTime(timehandler.getCurrentTime());
 			}
 		}
 	}
@@ -263,8 +249,8 @@ public class Parser {
 	private void setFromToDefaultTime(Task newTask) {
 		if (newTask.getTaskType().equals(TASKTYPE_EVENT)) {
 			if (newTask.taskStartDateIsEmpty() && newTask.taskEndDateIsEmpty()) {
-				newTask.setTaskStartDate(getCurrentDate());
-				newTask.setTaskEndDate(getCurrentDate());
+				newTask.setTaskStartDate(timehandler.getCurrentDate());
+				newTask.setTaskEndDate(timehandler.getCurrentDate());
 			}
 			if (newTask.taskStartDateIsEmpty() && !newTask.taskEndDateIsEmpty()) {
 				newTask.setTaskStartDate(newTask.getTaskEndDate());
@@ -290,53 +276,14 @@ public class Parser {
 		return date;
 	}
 
-	private String addOneDay(String date, String time) {
-		Calendar initialTime = Calendar.getInstance();
-		SimpleDateFormat dateFormatter = new SimpleDateFormat("ddMMyy HHmm");
-		time = getCurrentTime();
-		initialTime.set(Integer.parseInt(date.substring(4, 6)), Integer.parseInt(date.substring(2, 4)) - 1,
-				Integer.parseInt(date.substring(0, 2)), Integer.parseInt(time.substring(0, 2)),
-				Integer.parseInt(time.substring(2, 4)), 0);
-		initialTime.add(Calendar.HOUR, 24);
-		String[] timeInfo = dateFormatter.format(initialTime.getTime()).split(" ");
-		return timeInfo[0];
-
-	}
-
-	private String[] addOneHour(String date, String time) {
-		Calendar initialTime = Calendar.getInstance();
-		initialTime.set(Integer.parseInt(date.substring(4, 6)), Integer.parseInt(date.substring(2, 4)) - 1,
-				Integer.parseInt(date.substring(0, 2)), Integer.parseInt(time.substring(0, 2)),
-				Integer.parseInt(time.substring(2, 4)), 0);
-		initialTime.add(Calendar.HOUR, 1);
-		SimpleDateFormat dateFormatter = new SimpleDateFormat("ddMMyy HHmm");
-		String[] timeInfo = dateFormatter.format(initialTime.getTime()).split(" ");
-		return timeInfo;
-	}
-
-	public String getCurrentDate() {
-		DateFormat df = new SimpleDateFormat("ddMMyy");
-		Date dateobj = new Date();
-		String currentDate = df.format(dateobj);
-		return currentDate;
-	}
-
-	public String getCurrentTime() {
-		DateFormat df = new SimpleDateFormat("HHmm");
-		Calendar calobj = Calendar.getInstance();
-		String time = df.format(calobj.getTime());
-		return time;
-	}
-
 	// 0 for date 1 for time
 	private String[] getTimeInfo(String[] commandParts, Task newTask, int wordIndex) throws Exception {
-		Boolean commaIsPresent = false;
 		String[] timeInfo = new String[2];
 		timeInfo[0] = "-1";
 		timeInfo[1] = "-1";
 
-		String currentDate = getCurrentDate();
-		String currentTime = getCurrentTime();
+		String currentDate = timehandler.getCurrentDate();
+		String currentTime = timehandler.getCurrentTime();
 		String taskOn = commandParts[wordIndex];
 		// trim and comma is present
 		if (commandParts[wordIndex].contains(",")) {
@@ -361,80 +308,18 @@ public class Parser {
 				}
 				timeInfo[1] = taskOn;
 			} else if (taskOn.length() == 6) {
-				if (!isDateValid(taskOn)) {
+				if (!timehandler.isDateValid(taskOn)) {
 					throw new Exception("Invalid Date");
 				}
 				timeInfo[0] = taskOn;
 			} else if (taskOn.equals(KEYWORD_TOMORROW)) {
-				String nextDate = addOneDay(currentDate, currentTime);
+				String nextDate = timehandler.addOneDay(currentDate, currentTime);
 				timeInfo[0] = nextDate;
 			}
 			wordIndex++;
 		}
 
 		return timeInfo;
-	}
-
-	public boolean endDatePassed(String currentDate, String endDate) {
-
-		int currentDay = parseInt(getDay(currentDate));
-		int currentMonth = parseInt(getMonth(currentDate));
-		int currentYear = parseInt(getYear(currentDate));
-		int endDay = parseInt(getDay(endDate));
-		int endMonth = parseInt(getMonth(endDate));
-		int endYear = parseInt(getYear(endDate));
-
-		if ((currentYear > endYear)) {
-			return true;
-		} else if ((currentMonth > endMonth) && (currentYear == endYear)) {
-			return true;
-		} else if ((currentDay > endDay) && (currentMonth == endMonth) && (currentYear == endYear)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public String getDay(String date) {
-		return date.substring(0, 2);
-	}
-
-	public String getMonth(String date) {
-		return date.substring(2, 4);
-	}
-
-	public String getYear(String date) {
-		return date.substring(4, 6);
-	}
-
-	public String addOneYear(String date) {
-		int day = parseInt(getDay(date));
-		int month = parseInt(getMonth(date));
-		int year = parseInt(getYear(date)) + 1;
-
-		String newDate = "";
-		newDate = newDate.concat(toTwoDigit(Integer.toString(day)));
-		newDate = newDate.concat(toTwoDigit(Integer.toString(month)));
-		newDate = newDate.concat((Integer.toString(year)));
-
-		return newDate;
-	}
-
-	String toTwoDigit(String num) {
-		if (num.length() == 1) {
-			num = "0" + num;
-		}
-
-		return num;
-	}
-
-	String toSixDigit(int num) {
-		String numString = Integer.toString(num);
-		if (numString.length() == 5) {
-			numString = "0" + numString;
-		}
-
-		return numString;
 	}
 
 	public boolean nextPriorityIsHigher(String currentPriority, String nextPriority) {
@@ -448,37 +333,4 @@ public class Parser {
 		}
 	}
 
-	int parseInt(String str) {
-		int i = 0;
-		int num = 0;
-		boolean isNeg = false;
-
-		// Check for negative sign; if it's there, set the isNeg flag
-		if (str.charAt(0) == '-') {
-			isNeg = true;
-			i = 1;
-		}
-
-		// Process each character of the string;
-		while (i < str.length()) {
-			num *= 10;
-			num += str.charAt(i++) - '0'; // Minus the ASCII code of '0' to get
-											// the value of the charAt(i++).
-		}
-
-		if (isNeg)
-			num = -num;
-		return num;
-	}
-
-	public static boolean isDateValid(String date) {
-		try {
-			DateFormat df = new SimpleDateFormat("ddMMyy");
-			df.setLenient(false);
-			df.parse(date);
-			return true;
-		} catch (ParseException e) {
-			return false;
-		}
-	}
 }
